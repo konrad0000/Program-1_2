450a2b466b63e3f21f868b6607553d88c289bdf1
konrad00 <100691793+konrad0000@users.noreply.github.com>
2022-03-15 12:05:03 +0000

    Zad 2

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int liczba = 100;
+    System.out.println("Twoja liczba to : " + liczba);
   }
 }
